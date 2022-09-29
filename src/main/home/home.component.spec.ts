80cc298bd58ec5c9d25bd37d4de476000966c317
Cel101 <77995992+Cel101@users.noreply.github.com>
2022-09-29 15:06:26 -0400

    my first commit

--- src/main/home/home.component.spec.ts
+++ src/main/home/home.component.spec.ts
@@ -1,31 +1,31 @@
 import { TestBed } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        HomeComponent
       ],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(HomeComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angularproject1'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
+  it(`should have as title 'CoolKidsConnect'`, () => {
+    const fixture = TestBed.createComponent(HomeComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angularproject1');
+    expect(app.title).toEqual('CoolKidsConnect');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
+    const fixture = TestBed.createComponent(HomeComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angularproject1 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('CoolKidsConnect app is running!');
   });
 });
